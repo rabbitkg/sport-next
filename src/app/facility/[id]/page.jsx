c45147585fc0ceb684528185a5e9be1d68147ec6
rabbitkg <rabbitkgbd@gmail.com>
2026-05-22 05:19:38 +0600

    access token in server component

--- src/app/facility/[id]/page.jsx
+++ src/app/facility/[id]/page.jsx
@@ -1,10 +1,21 @@
 import FacilitiesDetailsPage from "@/components/FacilitiesDetailsPage";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 const FacilityDetailPage = async ({ params }) => {
     const { id } = await params;
 
+    const {token} = await auth.api.getToken({
+        headers: await headers()
+    })
+
+        
+
     const res = await fetch(`http://localhost:5000/facility/${id}`, {
         cache: "no-store",
+        headers: {
+            authorization: `Bearer ${token}`
+        }
     });
     const facility = await res.json();
 
